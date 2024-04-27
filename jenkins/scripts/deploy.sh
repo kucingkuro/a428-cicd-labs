92d9d37f310d9d46d4f5cabb67a3c865b42200cf
kuro <112769418+kucingkuro@users.noreply.github.com>
2024-04-27 15:42:42 +0700

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Skrip deploy.sh untuk deploy aplikasi ke lingkungan produksi
-
-# Memulai proses deploy
 echo "Memulai proses deploy..."
-
-# Contoh: Mengirimkan file yang akan di-deploy ke server produksi
-# Anda dapat menyesuaikan dengan prosedur deploy yang sesuai dengan lingkungan Anda
-# Misalnya: menggunakan rsync, scp, atau layanan CI/CD seperti AWS CodeDeploy
-
-# Contoh: Menggunakan rsync untuk mengirim file ke server produksi
-# rsync -avz --exclude='node_modules' ./path/to/your/application user@your-production-server:/path/to/deployment/directory
 
 echo "Proses deploy selesai."
